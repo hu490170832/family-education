b5d2993e1d7e462134919d1963935c10a158b75e
hucheng113 <hucheng113@pingan.com.cn>
2018-12-14 19:16:54 +0800

    filter

--- src/pages/index/components/filter/index.vue
+++ src/pages/index/components/filter/index.vue
@@ -1,90 +1,135 @@
 <template>
-  <div id="filter" class="filter" :class="{catch: catchUp}">
-    <div class="filter-tab">
-      <div class="tab-item">
-        <span class="txt">时薪高低</span>
-        <span class="icon iconfont icon-xiajiantou"></span>
-        <span class="line"></span>
+  <div id="filter" class="filter">
+    <div class="filter-fixd" :class="{show: catchUp}">
+      <div class="filter-tab">
+        <div class="tab-item">
+          <span class="txt">时薪高低</span>
+          <span class="icon iconfont icon-xiajiantou"></span>
+          <span class="line"></span>
+        </div>
+        <div class="tab-item">
+          <span class="txt">授课区域</span>
+          <span class="icon iconfont icon-xiajiantou"></span>
+          <span class="line"></span>
+        </div>
+        <div class="tab-item">
+          <span class="txt">筛选</span>
+          <span class="icon iconfont icon-xiajiantou"></span>
+        </div>
       </div>
-      <div class="tab-item">
-        <span class="txt">授课区域</span>
-        <span class="icon iconfont icon-xiajiantou"></span>
-        <span class="line"></span>
-      </div>
-      <div class="tab-item">
-        <span class="txt">筛选</span>
-        <span class="icon iconfont icon-xiajiantou"></span>
+      <div class="tab-content">
+        <div class="content-one">
+
+        </div>
       </div>
     </div>
-    <div class="tab-content">
-      <div class="content-one">
+    <div class="filter-static" :class="{hide: catchUp}">
+      <div class="filter-tab">
+        <div class="tab-item">
+          <span class="txt">时薪高低</span>
+          <span class="icon iconfont icon-xiajiantou"></span>
+          <span class="line"></span>
+        </div>
+        <div class="tab-item">
+          <span class="txt">授课区域</span>
+          <span class="icon iconfont icon-xiajiantou"></span>
+          <span class="line"></span>
+        </div>
+        <div class="tab-item">
+          <span class="txt">筛选</span>
+          <span class="icon iconfont icon-xiajiantou"></span>
+        </div>
+      </div>
+      <div class="tab-content">
+        <div class="content-one">
 
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        catchUp: false
-      }
-    },
-    created() {
-      const query = wx.createSelectorQuery()
-      query.select('#filter').boundingClientRect()
-      query.selectViewport().scrollOffset()
-      query.exec(res=>{
-        this.filterOffsetTop = res[0].top       // #the-id节点的上边界坐标
-      })
-    },
-    onPageScroll(e) {
+import { throttle } from '@/utils'
+export default {
+  data() {
+    return {
+      catchUp: false
+    };
+  },
+  created() {
+    const query = wx.createSelectorQuery()
+    query.select('.filter-static').boundingClientRect()
+    query.selectViewport().scrollOffset()
+    query.exec( res=> {
+      this.fillterOffsetTop = res[0].top
+    })
+  },
+  // onLoad() {
+  //   let observer = wx.createIntersectionObserver();
+  //   observer.relativeToViewport({ top: -50 }).observe(".filter-static", res => {
+  //     console.log(res);
+  //     // res.intersectionRect.top 相交区域
+  //     console.log(res.intersectionRect.top)
+  //     if (res.intersectionRect.top === 0) {
+  //       this.catchUp = true;
+  //     } else {
+  //       this.catchUp = false;
+  //     }
+  //   });
+  // },
+  onPageScroll(e) {
+    throttle(()=>{
       const scrollTop = e.scrollTop
-      if(scrollTop>this.filterOffsetTop) {
-        if(this.catchUp) return;
-        this.catchUp = true
+      if(scrollTop>this.fillterOffsetTop) {
+        this.catchUp = true;
       }else {
-        if(!this.catchUp) return;
-        this.catchUp = false
+        this.catchUp = false;
       }
-    },
-    methods: {
-    }
-  }
+    })
+  },
+  methods: {}
+};
 </script>
 
 <style lang='stylus' scoped>
-  .filter
-    &.catch
-      position fixed
-      top 0
-      width 100%
-      background #fff
-    .filter-tab
-      display flex
-      border-bottom 1px solid #e0e0e0
-      .tab-item
-        position relative
-        font-size 14px
-        color #333
-        display flex
-        width 33%
-        box-sizing border-box
-        justify-content center
-        align-items center
-        height 35px
-        .line
-          position absolute
-          right 0
-          top 50%
-          transform translateY(-50%)
-          width 1px
-          height 16px
-          background #ccc
-        .txt
-          margin-right 6px
-        .icon
-          position relative
-          top 1px
+.filter 
+  .filter-fixd 
+    position: fixed;
+    top: 0;
+    width: 100%;
+    background: #fff;
+    display none;
+    &.show
+      display block
+  .filter-static 
+    &.hide 
+      display none;
+  .filter-tab 
+    display: flex;
+    border-bottom: 1px solid #e0e0e0;
+
+    .tab-item 
+      position: relative;
+      font-size: 14px;
+      color: #333;
+      display: flex;
+      width: 33%;
+      box-sizing: border-box;
+      justify-content: center;
+      align-items: center;
+      height: 35px;
+      .line 
+        position: absolute;
+        right: 0;
+        top: 50%;
+        transform: translateY(-50%);
+        width: 1px;
+        height: 16px;
+        background: #ccc;
+      .txt 
+        margin-right: 6px;
+      .icon 
+        position: relative;
+        top: 1px;
 </style>
--- src/utils/index.js
+++ src/utils/index.js
@@ -18,7 +18,30 @@ export function formatTime (date) {
   return `${t1} ${t2}`
 }
 
+export function throttle (fn , interval) {
+  var _self = fn,
+  timer,
+  firstTime = true;
+  return function () {
+    var args = arguments,
+    _me = this;
+    if(firstTime) {
+        _self.apply(_me, args)
+        return firstTime = false
+    }
+    if(timer) {
+        return false
+    }
+    timer = setTimeout(function () {
+        clearTimeout(timer)
+        timer = null
+        _self.apply(_me,args)
+    }, interval || 500)
+  }
+}
+
 export default {
   formatNumber,
-  formatTime
+  formatTime,
+  throttle
 }
